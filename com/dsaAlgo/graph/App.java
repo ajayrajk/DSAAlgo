8c5857cb24d532d9e54062fc05fa03fdc5f60a64
Ajay RajPut <ajayhbti11@gmail.com>
2024-08-29 01:25:20 +0530

    test

---
+++ com/dsaAlgo/graph/App.java
@@ -0,0 +1,32 @@
+package com.dsaAlgo.graph;
+
+public class App {
+    private static int [] []adjacencyMatrix={
+            {0,2,4,0},
+            {0,0,0,3},
+            {0,0,0,0},
+            {0,0,0,0}
+    };
+
+    public static void main (String [] args){
+        //find all edges
+        for(int i=0 ; i< adjacencyMatrix.length ; i++){
+
+            for(int j=0; j< adjacencyMatrix.length ; j++){
+                if(adjacencyMatrix[i][j]!=0){
+                    System.out.println("Edge with weight : "+ adjacencyMatrix[i][j]);
+                }
+            }
+        }
+        System.out.println(adjacencyMatrix[0][2]);
+
+        Vertex a=new Vertex("A");
+        Vertex b=new Vertex("B");
+        Vertex c=new Vertex("C");
+        Vertex d=new Vertex("D");
+
+
+
+    }
+
+}
---
+++ com/dsaAlgo/graph/Vertex.java
@@ -0,0 +1,4 @@
+package com.dsaAlgo.graph;
+
+public class Vertex {
+}
