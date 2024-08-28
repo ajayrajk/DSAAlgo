5cd998252c54d1207563c9fa9db85403b42298bc
Ajay RajPut <ajayhbti11@gmail.com>
2024-08-29 03:06:50 +0530

    BFS Algo

---
+++ com/dsaAlgo/graph/BFSAlgo.java
@@ -0,0 +1,19 @@
+package com.dsaAlgo.graph;
+/*
+* procedure BFS(G , root) is
+* let Q be a queue
+* label root as explored
+*  Q.enqueue(root)
+*  while Q is not empty do
+*  v : Q.dequeue
+*  if v is goal then
+*  return v
+*  for all edges from v to w in G.adjacentEdges(v) do
+*  if w is not labeled as explored then
+*  label w as explored
+*  w.parent := v
+*  Q.enqueue(w)
+*
+* */
+public class BFSAlgo {
+}
